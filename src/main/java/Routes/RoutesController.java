1138efd3544f24b6c6f057fea5bd71b5118c082a
Cronus <matiasperrud@gmail.com>
2024-07-09 19:03:02 -0300

    primeira etapa - configurando o springboot

---
+++ src/main/java/Routes/RoutesController.java
@@ -0,0 +1,13 @@
+package Routes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RoutesController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
